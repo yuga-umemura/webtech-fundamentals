92595689908b9a42857ecc2de0f36e133be24503
Yuga Umemura <yugaumemura@YuganoMacBook-Air.local>
2025-01-01 16:55:27 +0900

    Add comment

--- simple-webserver2/main.go
+++ simple-webserver2/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// FileServer returns a handler that serves HTTP requests with the contents of the file system rooted at root.
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
